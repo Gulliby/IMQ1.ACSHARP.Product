f363afb37299045d48a30a3c7bc49c6f3eaf7002
sergei.dubovik <sergei.dubovik@thomsonreuters.com>
2017-04-05 18:06:43 +0300

    Task was implemented.

---
+++ IMQ1.ACSHARP.Product.Library.Tests.Unit/Shop/ShopItemTests.cs
@@ -0,0 +1,23 @@
+﻿using IMQ1.ACSHARP.Product.Library.Shop;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IMQ1.ACSHARP.Product.Library.Tests.Unit.Shop
+{
+    [TestClass]
+    public class ShopItemTests
+    {
+        [TestMethod]
+        public void ShopItems_WithEqualProperties_Are_Equal()
+        {
+            // arrange
+            var firstShopItem = new ShopItem("QWERTY", 20, 10);
+            var secondShopItem = new ShopItem("QWERTY", 20, 10);
+
+            // act
+            var result = firstShopItem.Equals(secondShopItem);
+
+            // assert
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ IMQ1.ACSHARP.Product.Library/Shop/ShopItem.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace IMQ1.ACSHARP.Product.Library.Shop
+{
+    /// <summary>
+    /// The entity that is responsible for storing information about the product.
+    /// </summary>
+    /// <seealso cref="System.IEquatable{ShopItem}" />
+    public struct ShopItem : IEquatable<ShopItem>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ShopItem"/> struct.
+        /// </summary>
+        /// <param name="name">Name of the product.</param>
+        /// <param name="cost">Cost of the product.</param>
+        /// <param name="count">Quantity of products.</param>
+        public ShopItem(string name, int cost, int count)
+        {
+            Name = name;
+            Cost = cost;
+            Count = count;
+        }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets the cost.
+        /// </summary>
+        /// <value>
+        /// The cost.
+        /// </value>
+        public int Cost { get; }
+
+        /// <summary>
+        /// Gets the count.
+        /// </summary>
+        /// <value>
+        /// The count.
+        /// </value>
+        public int Count { get; }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="object" />, is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="object" /> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="object" /> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is ShopItem)
+            {
+                var si = (ShopItem)obj;
+                return Equals(si);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns>
+        /// true if the current object is equal to the <paramref name="other" /> parameter; otherwise, false.
+        /// </returns>
+        public bool Equals(ShopItem other)
+        {
+            return Name == other.Name &&
+                   Cost == other.Cost &&
+                   Count == other.Count;
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return string
+                   .Format("{0} {1} {2}", Name, Cost, Count)
+                   .GetHashCode();
+        }
+    }
+}
